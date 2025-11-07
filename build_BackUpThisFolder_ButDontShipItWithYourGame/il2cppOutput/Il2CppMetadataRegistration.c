fb363bbbcd20654161b8de3b72de6e46ee015c1a
e22103349 <e22103349@etudiant.univ-brest.fr>
2025-11-07 15:32:35 +0100

    v3 labyrinthe

---
+++ build_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	12369,
+	g_Il2CppGenericTypes,
+	8775,
+	g_Il2CppGenericInstTable,
+	113431,
+	g_Il2CppGenericMethodFunctions,
+	37441,
+	g_Il2CppTypeTable,
+	131124,
+	g_Il2CppMethodSpecTable,
+	10417,
+	g_FieldOffsetTable,
+	10417,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
